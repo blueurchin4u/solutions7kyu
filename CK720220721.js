8c3ac7dbfb375c92f24cc2322cab3247aefc7cf5
blueurchin4u <93684449+blueurchin4u@users.noreply.github.com>
2022-07-21 13:20:07 -0500

    submitted You're a square! 7kyu solution

--- CK720220721.js
+++ CK720220721.js
@@ -1,7 +1,7 @@
 /*
 You're a square!
 
-Given an integral number, determine if it's a square number:
+Given an integral number, determine if it's a square number: 
 
 In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
 
