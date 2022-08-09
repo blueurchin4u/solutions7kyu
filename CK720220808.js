658a32e428240c6728cf01ea21c98f1518cdfd2c
blueurchin4u <93684449+blueurchin4u@users.noreply.github.com>
2022-08-08 23:50:30 -0500

    submitted Friend or Foe? 7kyu solution

---
+++ CK720220808.js
@@ -0,0 +1,19 @@
+/*
+Friend or Foe?
+
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e.
+
+friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+Note: keep the original order of the names in the output.
+
+The filter() method creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that pass the test implemented by the provided function.
+*/
+
+const friend = friends => friends.filter(el => el.length === 4);
+
