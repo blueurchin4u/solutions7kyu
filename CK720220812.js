3944c02fc34a87c6cfb90fc58f7a77cbbd252a88
blueurchin4u <93684449+blueurchin4u@users.noreply.github.com>
2022-08-12 00:27:50 -0500

    submitted Make a function that does arithmetic 7kyu solution

--- CK720220812.js
+++ CK720220812.js
@@ -8,7 +8,7 @@ a and b will both be positive integers, and a will always be the first number in
 The four operators are "add", "subtract", "divide", "multiply".
 
 A few examples:(Input1, Input2, Input3 --> Output)
-
+ 
 5, 2, "add"      --> 7
 5, 2, "subtract" --> 3
 5, 2, "multiply" --> 10
