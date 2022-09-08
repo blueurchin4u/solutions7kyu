4098d67b8307f2de73b88838e37261792c9d8327
blueurchin4u <93684449+blueurchin4u@users.noreply.github.com>
2022-09-08 13:04:21 -0500

    submitted Remove anchor from URL 7kyu solution

---
+++ CK720220908.js
@@ -0,0 +1,11 @@
+/*
+Remove anchor from URL
+
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+*/
+
+const removeUrlAnchor = url = url => url.split('#', 1)[0]
